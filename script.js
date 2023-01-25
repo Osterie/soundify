6e333758c8eb33d49bc5234e457e6328aa480264
OSTERITE <adrian@bandidos.org>
2023-01-25 10:59:21 +0100

    spill av og modus improvements

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ let nåværende_spilleliste = new Spilleliste();
 
 window.onload = winInit;
 function winInit(){ 
-
     hendelser()
 }
 
 function hendelser(){
 
-    //henter DOM elementer med klasse spilleliste
+    //henter DOM elementer med klasse lik "spilleliste"
     let spillelister_navn = document.querySelectorAll('.spilleliste')
 
-    // gir DOM elementene eventlistener click
+    //gir DOM elementene eventlistener click
     for (let i = 0; i < spillelister_navn.length; i++) {
         spillelister_navn[i].addEventListener('click', function() {
             //ved å bruke this er det som å bruke variabelen spillelister_navn[i]
